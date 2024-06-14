0ef957461f0b0c1b0832545e1539cbbd8dafe562
zero2exp <zero2exp@proton.me>
2024-06-14 12:04:48 -0600

    update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def main():
     text = get_book_text(book_path)
     words = count_words(text)
     chars = get_chars_dict(text)
+    list_of_chars = [{'key': k, 'value':v} for k, v in chars.items()]
     print(text)
     print(f"{words} words found in the document")
-    print(chars)
+    list_of_chars.sort(reverse=True, key=sort_on)
+    print(list_of_chars)
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -24,5 +26,6 @@ def get_chars_dict(text):
         else:
             char_count[char] = 1
     return char_count
-
+def sort_on(dict):
+    return dict['value']
 main()
